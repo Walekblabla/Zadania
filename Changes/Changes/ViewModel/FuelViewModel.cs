b92b7ea707a4be477de9cac562c7280b861b10e0
Jan Kowalski <luca.r@wp.pl>
2017-11-18 09:50:02 +0100

    commit2

--- Changes/Changes/ViewModel/FuelViewModel.cs
+++ Changes/Changes/ViewModel/FuelViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Changes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Changes.ViewModel
 {
-    public class FuelModel
+    public class FuelViewModel
     {
         public FuelModel FuelModel { get; set; }
         public bool ShowResult { get; set; }
