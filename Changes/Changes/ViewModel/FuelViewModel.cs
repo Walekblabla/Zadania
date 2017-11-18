879874d8a4758abf2a83b91bcd8586bef6ac18e6
Jan Kowalski <luca.r@wp.pl>
2017-11-18 09:48:38 +0100

    commit1

---
+++ Changes/Changes/ViewModel/FuelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Changes.ViewModel
+{
+    public class FuelModel
+    {
+        public FuelModel FuelModel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
